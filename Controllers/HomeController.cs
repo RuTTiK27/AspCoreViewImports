bbed48c6c5257555a8f5948b7816f3f9e897177e
Rutvik Pandya <rutvikpandya00@gmail.com>
2024-11-09 14:16:06 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using AspCoreViewImports.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspCoreViewImports.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student{Id=1,Name="Rutvik",Gender="Male" },
+                new Student{Id=2,Name="Karo",Gender="Male" },
+                new Student{Id=3,Name="Kaka",Gender="Male" }
+            };
+            return View(students);
+        }
+        public IActionResult About()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student{Id=1,Name="Rutvik",Gender="Male" },
+                new Student{Id=2,Name="Karo",Gender="Male" },
+                new Student{Id=3,Name="Kaka",Gender="Male" }
+            };
+            return View(students);
+        }
+        public IActionResult Contact()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student{Id=1,Name="Rutvik",Gender="Male" },
+                new Student{Id=2,Name="Karo",Gender="Male" },
+                new Student{Id=3,Name="Kaka",Gender="Male" }
+            };
+            return View(students);
+        }
+    }
+}
